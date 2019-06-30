c8e3523129c86e522fae8de0035255ba2ac27d8d
Valentin <vavalik999@gmail.com>
2019-06-30 13:07:13 +0300

    2

--- ever/conn.php
+++ ever/conn.php
@@ -1,7 +1,12 @@
 <?php
 	
-	define('HOST', 'vseti');
+	/*define('HOST', 'vseti');
 	define('USER', 'root');
 	define('PASSWORD', '');
 	define('BD', 'vseti');
+	*/
+	define('HOST', 'ec2-54-228-246-214.eu-west-1.compute.amazonaws.com');
+	define('USER', 'tizahfcrzrgynl');
+	define('PASSWORD', 'd814b59ae9038ac5632173fc5219e96adedc7202593b84d4ae70aa5fc19a4b62');
+	define('BD', 'db63qheeq6sg9');
 	?>
